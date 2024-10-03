f0d91cd3df17c16e1af7368b91078d7bcbf32465
heinriej <heinirej@mail.uc.edu>
2024-10-03 14:41:32 -0400

    1/4 commit

--- src/CtoF_Converter.java
+++ src/CtoF_Converter.java
@@ -5,5 +5,29 @@ public class CtoF_Converter
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        double celsius = 0;
+        double fahrenheit = 0;
+        boolean doneTemp = false;
+        String trash = "";
+
+
+        do
+        {
+            System.out.print("Enter your temperature in Celsius: ");
+            if (in.hasNextDouble())
+            {
+                celsius = in.nextDouble();
+                in.nextLine();
+                fahrenheit = (celsius * 1.8) + 32;
+                doneTemp = true;
+                System.out.println("Your temperature in Fahrenheit is " + fahrenheit);
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You entered " + trash + " try again!");
+            }
+
+        }while(!doneTemp);
     }
 }
