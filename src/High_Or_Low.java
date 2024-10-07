2d360e635af4f7b88fa2f7a10edd530e9734c1d8
heinriej <heinirej@mail.uc.edu>
2024-10-07 12:17:32 -0400

    3/4 commit

--- src/High_Or_Low.java
+++ src/High_Or_Low.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class High_Or_Low
@@ -5,5 +6,49 @@ public class High_Or_Low
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        Random generator = new Random();
+        int val = generator.nextInt(10) + 1;
+        int guess = 0;
+        boolean doneGuess = false;
+        boolean doneCorrect = false;
+        String trash = "";
+
+        do
+        {
+            System.out.print("Guess the random number [1-10]: ");
+            if (in.hasNextInt())
+            {
+                guess = in.nextInt();
+                in.nextLine();
+                if (guess > val & guess <= 10)
+                {
+                    System.out.println("Your guess " + guess + " was too high! The correct number was " + val);
+                    doneCorrect = true;
+                    doneGuess = true;
+                }
+                else if (guess < val & guess >= 1)
+                {
+                    System.out.println("Your guess " + guess + " was too low! The correct number was " + val);
+                    doneCorrect = true;
+                    doneGuess = true;
+                }
+                else if (guess >= 11 || guess <= -1)
+                {
+                    System.out.println("Your guess " + guess + " is not in bounds!");
+                }
+                else
+                {
+                    System.out.println("Your guess " + guess + " was correct! The correct number was " + val);
+                    doneCorrect = true;
+                    doneGuess = true;
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You entered " + trash + " try again!");
+            }
+        }while(!doneGuess);
+
     }
 }
